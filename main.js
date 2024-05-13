67ff4e45f26dcb28af1053cbf7a8b60605b5b310
JhenyFranchii <167088886+JhenyFranchii@users.noreply.github.com>
2024-05-13 09:23:46 -0300

    j

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-11-03T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-25T00:00:00");
+const tempoObjetivo3 = new Date("2024-09-17T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-14T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
